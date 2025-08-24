0b4fdcd677d2feddfaaa2d871d821c5b93d59800
Mohammad <mohammad.ah.pr@gmail.com>
2025-08-24 15:17:19 +0330

    configure create tables def

---
+++ app.py
@@ -0,0 +1,37 @@
+import mysql.connector
+from contextlib import contextmanager
+from constants import connectionDetail,tables
+@contextmanager
+def dbConnection():
+    conn = None
+    cursor = None
+    try:
+        conn = mysql.connector.connect(
+            host=connectionDetail['host'],
+            port=connectionDetail['port'],
+            user=connectionDetail['user'],
+            password=connectionDetail['password'],
+            database=connectionDetail['database']
+        )
+        cursor = conn.cursor()
+        conn.autocommit=True
+        yield cursor
+
+    except Exception as e:
+        print("An error occurred:", e)
+        if conn:
+            conn.rollback()
+        return False
+
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+        print('Connection closed')
+
+def createTables():
+    with dbConnection as cursor:
+        for table in tables:
+            query=tables[table]
+            cursor.execute(query)
--- bot.py
+++ bot.py
@@ -2,8 +2,10 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ChatPermissions
 from constants import Token
 from handlers import register_handlers
+from app import createTables
 
 bot = telebot.TeleBot(Token)
+createTables()
 register_handlers(bot)
 if __name__ == "__main__":
     bot.infinity_polling()
